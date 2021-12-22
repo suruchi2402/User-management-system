9fd0b787c4fd7b9bf19153b0627ba9adcf370abb
suruchi <pinkp7840@gmail.com>
2021-12-22 16:55:01 +0530

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
-
+const dotenv=require('dotenv');
+dotenv.config();
 //Connection pool
 const pool = mysql.createPool({
     connectionLimit: 100,
--- user-schema.sql
+++ user-schema.sql
@@ -1,4 +1,7 @@
-CREATE TABLE `usermanagement_tut`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `usermanagement_tut`.`user` 
+( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL ,
+ `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL ,
+  `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 -- INSERT INTO `user`
 -- (`id`, `first_name`,`last_name`,`email`, `phone` , `comments`, `status`) VALUES
